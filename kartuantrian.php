c3197df5d248e244a2d815cc31cab9f163cd30df
azharpr <onesyak19@gmail.com>
2026-05-11 14:38:47 +0700

    Update kartuantrian.php

--- kartuantrian.php
+++ kartuantrian.php
@@ -36,7 +36,6 @@ while($row = mysqli_fetch_assoc($query)) {
     <div class="main-content">
       <div class="header">SEMUA KARTU ANTRIAN</div>
       <div class="form-container">
-        
         <?php if (count($semua_antrian) > 0): ?>
             <div class="cards-wrapper">
                 <?php foreach ($semua_antrian as $tiket): ?>
